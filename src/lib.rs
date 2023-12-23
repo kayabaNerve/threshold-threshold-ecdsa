d5e708c172c1a79d4e6a95e82198032b8cdc3cbe
Luke Parker <lukeparker5132@gmail.com>
2023-12-23 02:19:31 -0500

    Add test for ECDSA rewrite

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#[cfg(test)]
+mod tests {
+  use rand_core::OsRng;
+  use elliptic_curve::point::AffineCoordinates;
+  use ciphersuite::{group::{ff::{Field, PrimeField}, Group}, Ciphersuite, Secp256k1};
+
+  fn verify(public_key: <Secp256k1 as Ciphersuite>::G, message_hash: <Secp256k1 as Ciphersuite>::F, r: <Secp256k1 as Ciphersuite>::F, s: <Secp256k1 as Ciphersuite>::F) {
+    assert_ne!(r, <Secp256k1 as Ciphersuite>::F::ZERO);
+    assert_ne!(s, <Secp256k1 as Ciphersuite>::F::ZERO);
+
+    let z = message_hash;
+    let u1 = z * s.invert().unwrap();
+    let u2 = r * s.invert().unwrap();
+    let point = (Secp256k1::generator() * u1) + (public_key * u2);
+    assert!(!bool::from(point.is_identity()));
+    assert_eq!(r.to_repr(), point.to_affine().x());
+  }
+
+  #[test]
+  fn non_distributed() {
+    let private_key = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+
+    let x = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let y = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let z = x * y;
+    let d = private_key * y;
+
+    let r = <Secp256k1 as Ciphersuite>::F::from_repr((Secp256k1::generator() * x).to_affine().x()).unwrap();
+
+    let message_hash = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let w = (message_hash * y) + (r * d);
+    let s = w * z.invert().unwrap();
+
+    verify(Secp256k1::generator() * private_key, message_hash, r, s);
+  }
+}
