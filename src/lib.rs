af8a6295b5b93a53b95db3fcb5d26ce9872e4cff
Luke Parker <lukeparker5132@gmail.com>
2023-12-23 04:24:53 -0500

    Paillier

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,74 @@
+use ciphersuite::{
+  group::{ff::PrimeField, Group},
+  Ciphersuite, Secp256k1,
+};
+use elliptic_curve::point::AffineCoordinates;
+
+pub mod paillier;
+
+pub fn verify(
+  public_key: <Secp256k1 as Ciphersuite>::G,
+  message_hash: <Secp256k1 as Ciphersuite>::F,
+  r: <Secp256k1 as Ciphersuite>::F,
+  s: <Secp256k1 as Ciphersuite>::F,
+) {
+  assert_ne!(r, <Secp256k1 as Ciphersuite>::F::ZERO);
+  assert_ne!(s, <Secp256k1 as Ciphersuite>::F::ZERO);
+
+  let z = message_hash;
+  let u1 = z * s.invert().unwrap();
+  let u2 = r * s.invert().unwrap();
+  let point = (Secp256k1::generator() * u1) + (public_key * u2);
+  assert!(!bool::from(point.is_identity()));
+  assert_eq!(r.to_repr(), point.to_affine().x());
+
+  ecdsa::hazmat::verify_prehashed(
+    &public_key,
+    &message_hash.to_repr(),
+    &ecdsa::Signature::<k256::Secp256k1>::from_scalars(r, s).unwrap(),
+  )
+  .unwrap()
+}
+
 #[cfg(test)]
 mod tests {
-    use ciphersuite::{
-        group::{
-            ff::{Field, PrimeField},
-            Group,
-        },
-        Ciphersuite, Secp256k1,
-    };
-    use elliptic_curve::point::AffineCoordinates;
-    use rand_core::OsRng;
+  use rand_core::OsRng;
+  use super::*;
 
-    fn verify(
-        public_key: <Secp256k1 as Ciphersuite>::G,
-        message_hash: <Secp256k1 as Ciphersuite>::F,
-        r: <Secp256k1 as Ciphersuite>::F,
-        s: <Secp256k1 as Ciphersuite>::F,
-    ) {
-        assert_ne!(r, <Secp256k1 as Ciphersuite>::F::ZERO);
-        assert_ne!(s, <Secp256k1 as Ciphersuite>::F::ZERO);
+  #[test]
+  fn paillier() {
+    use crypto_bigint::*;
+    use paillier::*;
 
-        let z = message_hash;
-        let u1 = z * s.invert().unwrap();
-        let u2 = r * s.invert().unwrap();
-        let point = (Secp256k1::generator() * u1) + (public_key * u2);
-        assert!(!bool::from(point.is_identity()));
-        assert_eq!(r.to_repr(), point.to_affine().x());
-
-        ecdsa::hazmat::verify_prehashed(
-            &public_key,
-            &message_hash.to_repr(),
-            &ecdsa::Signature::<k256::Secp256k1>::from_scalars(r, s).unwrap(),
-        )
-        .unwrap()
+    let (private, public) = PrivateKey::new(&mut OsRng);
+    println!("Created private key in paillier test");
+    loop {
+      let message = Uint::<{ N_LIMBS }>::random(&mut OsRng);
+      if let Some(ciphertext) = public.encrypt(&mut OsRng, message) {
+        assert_eq!(message, private.decrypt(ciphertext).unwrap());
+        break;
+      }
     }
+  }
 
-    #[test]
-    fn non_distributed() {
-        let private_key = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+  #[test]
+  fn non_distributed() {
+    use ciphersuite::group::ff::Field;
 
-        let x = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
-        let y = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
-        let z = x * y;
-        let d = private_key * y;
+    let private_key = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
 
-        let r =
-            <Secp256k1 as Ciphersuite>::F::from_repr((Secp256k1::generator() * x).to_affine().x())
-                .unwrap();
+    let x = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let y = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let z = x * y;
+    let d = private_key * y;
 
-        let message_hash = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
-        let w = (message_hash * y) + (r * d);
-        let s = w * z.invert().unwrap();
+    let r = <Secp256k1 as Ciphersuite>::F::from_repr((Secp256k1::generator() * x).to_affine().x())
+      .unwrap();
 
-        verify(Secp256k1::generator() * private_key, message_hash, r, s);
-    }
+    let message_hash = <Secp256k1 as Ciphersuite>::F::random(&mut OsRng);
+    let w = (message_hash * y) + (r * d);
+    let s = w * z.invert().unwrap();
+
+    verify(Secp256k1::generator() * private_key, message_hash, r, s);
+  }
 }
---
+++ src/paillier.rs
@@ -0,0 +1,103 @@
+use rand_core::{RngCore, CryptoRng};
+
+use subtle::{ConstantTimeEq, ConstantTimeGreater};
+use crypto_bigint::{
+  CheckedAdd, CheckedSub, Random, Uint,
+  modular::runtime_mod::{DynResidueParams, DynResidue},
+};
+use crypto_primes::generate_safe_prime_with_rng;
+
+#[cfg(test)]
+pub const PRIME_BITS: usize = 1024;
+#[cfg(not(test))]
+pub const PRIME_BITS: usize = 1536;
+pub const N_BITS: usize = PRIME_BITS * 2;
+pub const N_2_BITS: usize = N_BITS * 2;
+
+// TODO: Assert limb size is 64-bits
+pub const PRIME_LIMBS: usize = PRIME_BITS / 64;
+pub const N_LIMBS: usize = N_BITS / 64;
+pub const N_2_LIMBS: usize = N_2_BITS / 64;
+
+#[derive(Clone)]
+pub struct PublicKey {
+  n: Uint<N_LIMBS>,
+  g: Uint<N_LIMBS>,
+}
+
+#[derive(Clone)]
+pub struct PrivateKey {
+  lambda: Uint<N_LIMBS>,
+  mu: Uint<N_LIMBS>,
+  public_key: PublicKey,
+}
+
+impl PrivateKey {
+  pub fn new(rng: &mut (impl RngCore + CryptoRng)) -> (PrivateKey, PublicKey) {
+    assert_eq!(Uint::<PRIME_LIMBS>::BITS, PRIME_BITS);
+
+    let p: Uint<PRIME_LIMBS> = generate_safe_prime_with_rng(&mut *rng, None);
+    let q: Uint<PRIME_LIMBS> = generate_safe_prime_with_rng(rng, None);
+    let n = p * q;
+
+    let g = n.checked_add(&Uint::ONE).unwrap();
+    let phi = p.checked_sub(&Uint::ONE).unwrap().mul(&q.checked_sub(&Uint::ONE).unwrap());
+    let lambda = phi;
+    let (mu, mu_valid) = DynResidue::new(&phi, DynResidueParams::new(&n)).invert();
+    assert!(bool::from(mu_valid));
+
+    let public = PublicKey { n, g };
+    (PrivateKey { lambda, mu: mu.retrieve(), public_key: public.clone() }, public)
+  }
+
+  #[allow(clippy::result_unit_err)]
+  pub fn decrypt(&self, ciphertext: Uint<N_2_LIMBS>) -> Result<Uint<N_LIMBS>, ()> {
+    let n_squared = DynResidueParams::new(&self.public_key.n.square());
+    #[allow(non_snake_case)]
+    let L_inner =
+      DynResidue::new(&ciphertext, n_squared).pow(&self.lambda.mul(&Uint::ONE)).retrieve();
+    let Some(x_minus_one) = Option::<Uint<N_2_LIMBS>>::from(L_inner.checked_sub(&Uint::ONE)) else {
+      Err(())?
+    };
+    #[allow(non_snake_case)]
+    let L_result_wide = x_minus_one.checked_div(&self.public_key.n.mul(&Uint::ONE)).unwrap();
+    #[allow(non_snake_case)]
+    let (L_result_high, L_result) = L_result_wide.split();
+    assert_eq!(L_result_high, Uint::ZERO);
+
+    let n = DynResidueParams::new(&self.public_key.n);
+    Ok(DynResidue::new(&L_result, n).mul(&DynResidue::new(&self.mu, n)).retrieve())
+  }
+}
+
+impl PublicKey {
+  pub fn encrypt(
+    &self,
+    rng: &mut (impl RngCore + CryptoRng),
+    message: Uint<N_LIMBS>,
+  ) -> Option<Uint<N_2_LIMBS>> {
+    if bool::from(message.ct_gt(&self.n) | message.ct_eq(&self.n)) {
+      None?;
+    }
+
+    let r = loop {
+      let candidate = Uint::<N_LIMBS>::random(&mut *rng);
+      if bool::from(
+        candidate.ct_eq(&Uint::ZERO) | candidate.ct_gt(&self.n) | candidate.ct_eq(&self.n),
+      ) {
+        continue;
+      }
+      break candidate;
+    };
+
+    let n_squared = DynResidueParams::new(&self.n.square());
+    let g_mod_n_squared = DynResidue::new(&self.g.mul(&Uint::ONE), n_squared);
+    let r_mod_n_squared = DynResidue::new(&r.mul(&Uint::ONE), n_squared);
+    Some(
+      g_mod_n_squared
+        .pow(&message.mul(&Uint::ONE))
+        .mul(&r_mod_n_squared.pow(&self.n.mul(&Uint::ONE)))
+        .retrieve(),
+    )
+  }
+}
